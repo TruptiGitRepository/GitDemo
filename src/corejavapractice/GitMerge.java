8958b16e0365125222909223eda1c9d9e730be0b
Trupti <d_trupti22@yahoo.com>
2020-08-26 11:13:45 -0400

    gitmerge

---
+++ src/corejavapractice/GitMerge.java
@@ -0,0 +1,11 @@
+package corejavapractice;
+
+public class GitMerge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("For git merge demo to develop2 branch");
+
+	}
+
+}
